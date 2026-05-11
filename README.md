ae236b98609e6af6f2a763d23ebfb7a4a4653541
不在乎y <ying_33@live.cn>
2026-05-11 21:24:57 +0800

    重构代码，移除第三方依赖，并修复之前一些事件丢失问题，因为找到了真正的原因。

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
-zed中dart无法使用`.`来自动完成代码。
+修复Zed中dart无法使用`.`来自动完成代码。
 
 ### 原因分析
 
-这个问题我一开始以为就是这样的，但搜索issue发现别人也有，在最新版本的zed中也有这个问题。于是我分析了下，发现是dart lsp在初始的时候返回的结果中未包含`triggerCharacters`字段，zed认为不需要。进一步测试发现dart lsp初始时返回的数据是很少的，与实际zed中获取到的dart lsp初始数据相差甚远，经过分析dart的代码(`dart-sdk-main\sdk-main\pkg\analysis_server\lib\src\lsp\constants.dart:dartCompletionTriggerCharacters`和`dart-sdk-main\sdk-main\pkg\analysis_server\lib\src\lsp\handlers\handler_completion.dart:CompletionRegistrations`)，似乎是通过其它事件动态注册的的，可能Zed并没有处理那个或者什么其它情况下忽略掉这个了吧，所以`.`无效。
+2026/5/11重新分析后得出结论：
 
-```json
-"completionProvider":{
-  "resolveProvider":true,
-  "triggerCharacters":["."]
-} 
-```
+**这是一个`Zed的bug`，在dart向客户端（Zed）注册事件`client/registerCapability`中，dart返回的结果中发现此事件中存在多个`textDocument/completion`节点，其中有未包含`triggerCharacters`的字段，而且在有字段的后面，而Zed在处理的时候是使用的覆盖模式，造成前一个结果被后面的null字段给替换了。**
 
-我尝试使用dart写了一个包装器，经过验证可以正常按`.`弹出，当然这只是一个实验，用来分析原因的，具体还得zed这边看能否解决，因为vs code和android studio并没有这样的问题。
+### 编译（因为只测试了Windows下的）
 
-### 编译Windows下的（因为只测试了Windows下的）
+**无第三方依赖，可以直接编译，之前版本的已经在macOS下编译测试过了**
 
 ```shell 
 dart compile exe bin/lsp_helper.dart
@@ -39,7 +34,10 @@ dart compile exe bin/lsp_helper.dart
 }
 ```
 
-
 ### 视频演示
 
-可以看[QQ录屏20260208162526.mp4](QQ录屏20260208162526.mp4)的效果
+可以看[效果演示](video.mp4)的效果
+<video width="320" height="240" controls>
+  <source src="video.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
--- bin/lsp_helper.dart
+++ bin/lsp_helper.dart
@@ -1,25 +1,28 @@
 //ignore_for_file: empty_catches
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:lsp_server/lsp_server.dart';
-// 测试完后就注释掉
-// import 'package:json_rpc_2/json_rpc_2.dart';
 // import 'package:win32/win32.dart';
 // import 'package:ffi/ffi.dart';
+import 'dart:math' as math;
 
 /// 测试用，Windows下使用Dgbview.exe或者CnDebugViewer.exe来捕获数据，测试完后就可以不要这个函数了。
 ///
 /// 代码是使用VS Code写的，Zed只是用来测试
-void printDebug(Object? object) {
-  if (Platform.isWindows && object != null) {
-    // final ptr = "$object".toNativeUtf16();
-    // try {
-    //   OutputDebugString(ptr);
-    // } finally {}
-    // free(ptr);
-  }
+void printDebug(Object? object) async {
+  // if (Platform.isWindows && object != null) {
+  //   try {
+  //     final ptr = "$object".toNativeUtf16();
+  //     try {
+  //       OutputDebugString(ptr);
+  //     } finally {
+  //       free(ptr);
+  //     }
+  //   } catch (e) {
+  //     printDebug("异常=$e");
+  //   }
+  // }
 }
 
 /// 要启动的dart进程文件名（短文件名），比如`dart.bat`
@@ -45,6 +48,114 @@ String? getDartFullPath() {
   return null;
 }
 
+/// LSP解码
+class LspMessageDecoder {
+  static const contentLength = 'Content-Length:';
+  static const contentType =
+      'Content-Type: application/vscode-jsonrpc; charset=utf-8';
+  // 提取正文内容长度
+  static final lengthRegEx = RegExp(r'Content-Length:\s*(\d+)');
+
+  final _buffer = <int>[];
+
+  /// 添加片断
+  void addChunk(Iterable<int> iterable) => _buffer.addAll(iterable);
+
+  /// 尝试解析，并通知监听的
+  void tryParseToSink(EventSink<Map<String, dynamic>> sink) {
+    final message = tryParse();
+    if (message == null) return;
+    sink.add(message);
+  }
+
+  /// 写lsp消息
+  static writeMessage(IOSink stream, dynamic message) async {
+    if (message == null) return;
+    // 注意文本这块，一定要先编码成utf-8的，否则会造成其它问题
+    final text = utf8.encode(message is String ? message : jsonEncode(message));
+    stream.add(
+        utf8.encode("$contentLength ${text.length}\r\n$contentType\r\n\r\n"));
+    stream.add(text);
+    await stream.flush();
+  }
+
+  /// 解析缓冲区完整 LSP 帧
+  Map<String, dynamic>? tryParse() {
+    try {
+      final headerEnd = _findHeaderEnd();
+      // 头部不完整，等待更多数据
+      if (headerEnd == -1) return null;
+      // 解析 Content-Length
+      final headerStr = utf8.decode(_buffer.sublist(0, headerEnd));
+      // 内容长度
+      final contentLength = _parseContentLength(headerStr);
+      if (contentLength <= 0) {
+        // 异常帧，丢弃头部
+        _buffer.removeRange(0, math.min(headerEnd + 4, _buffer.length));
+        return null;
+      }
+      // 报文整体长度 = 头部长度 + 分隔符\r\n\r\n + json长度
+      final totalFrameSize = headerEnd + 4 + contentLength;
+      if (_buffer.length < totalFrameSize) return null;
+      // 取json数据
+      final jsonBytes = _buffer.sublist(headerEnd + 4, totalFrameSize);
+      // 移除一个message
+      _buffer.removeRange(0, totalFrameSize);
+      // 解码JSON
+      return jsonDecode(utf8.decode(jsonBytes));
+    } catch (e, s) {
+      printDebug("_parseBuffer  exception: $e, \n $s");
+    }
+    return null;
+  }
+
+  /// 查找 \r\n\r\n 帧头结束位置
+  int _findHeaderEnd() {
+    for (int i = 0; i < _buffer.length; i++) {
+      if (_buffer[i] == 13 &&
+          i + 3 < _buffer.length &&
+          (_buffer[i + 1] == 10 &&
+              _buffer[i + 2] == 13 &&
+              _buffer[i + 3] == 10)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  /// 解析 Content-Length 数值
+  int _parseContentLength(String header) {
+    final match = lengthRegEx.firstMatch(header);
+    if (match == null) return 0;
+    return int.tryParse(match.group(1)!) ?? 0;
+  }
+}
+
+/// LSP 协议帧解码Transformer
+class LspMessageTransformer
+    extends StreamTransformerBase<List<int>, Map<String, dynamic>> {
+  final LspMessageDecoder decoder = LspMessageDecoder();
+
+  @override
+  Stream<Map<String, dynamic>> bind(Stream<List<int>> stream) {
+    return stream.transform(
+      StreamTransformer<List<int>, Map<String, dynamic>>.fromHandlers(
+        handleData: (data, sink) {
+          decoder.addChunk(data);
+          decoder.tryParseToSink(sink);
+        },
+        handleDone: (sink) {
+          decoder.tryParseToSink(sink);
+          sink.close();
+        },
+        handleError: (error, stackTrace, sink) =>
+            sink.addError(error, stackTrace),
+      ),
+    );
+  }
+}
+
+var initialized = false;
 void main(List<String> args) async {
   // 启动dart进程
   final dartProcess = await Process.start(
@@ -52,167 +163,41 @@ void main(List<String> args) async {
     ['language-server', '--protocol=lsp'],
     mode: ProcessStartMode.normal,
   );
-  // 桥接的中间lsp服务
-  var lspBridge = Connection(stdin, stdout);
-  // 连接原dart的lsp服务
-  var dartLsp = Connection(dartProcess.stdout, dartProcess.stdin);
-
-  // dart lsp请求简化
-  dynamic sendDartLspRequest(String method, dynamic params) async {
-    printDebug("收到zed消息：method=$method, value=$params");
+  // 重定向dart进程的管道
+  dartProcess.stderr.pipe(stderr);
+  stdin.pipe(dartProcess.stdin);
+  // 监听dart stdout
+  dartProcess.stdout.transform(LspMessageTransformer()).listen((message) async {
     try {
-      final res = await dartLsp.sendRequest(method, params);
-      printDebug(
-          "请求dart成功，返回结果：method=$method, result=$res, type=${res?.runtimeType}");
-      return res;
-    } catch (e) {
-      // TODO: 这地方感觉不太对，光屏蔽异常？是否需要返回给zed???
-      printDebug("请求dart错误 method=$method, value=$params, 异常=$e");
+      //printDebug("dartProcess.stdout message=$message");
+      if (!initialized) {
+        // 经过研究，发现此事件中存在多个`textDocument/completion`, 在dart返回的结果中
+        // 有定义`triggerCharacters`字段的在前面，zed在处理的时候是使用的覆盖模式，造成
+        // 结果被替换为null了。
+        if (message['method'] == 'client/registerCapability') {
+          final registrations = message['params']?['registrations'];
+          List? oldtriggerCharacters;
+          if (registrations != null && registrations is List) {
+            for (final item in registrations) {
+              if (item['method'] == "textDocument/completion") {
+                final triggerCharacters =
+                    item['registerOptions']?['triggerCharacters'];
+                if (triggerCharacters != null) {
+                  oldtriggerCharacters = triggerCharacters;
+                } else {
+                  item['registerOptions']?['triggerCharacters'] =
+                      oldtriggerCharacters;
+                }
+              }
+            }
+          }
+          initialized = true;
+        }
+      }
+      await LspMessageDecoder.writeMessage(stdout, message);
+      return;
+    } catch (e, s) {
+      printDebug("object stdout exception: $e\n$s");
     }
-    return null;
-  }
-
-  // 监听dart lsp的主动消息？？？
-  dartLsp.peer.registerFallback((parameters) {
-    printDebug(
-        "dartLsp method=${parameters.method}, value=${parameters.value}");
-    //TODO: 这里应该这样做？？？？我也不知道！
-    return lspBridge.sendNotification(parameters.method, parameters.value);
   });
-
-  // 转发其它请求
-  lspBridge.peer.registerFallback((parameters) async =>
-      sendDartLspRequest(parameters.method, parameters.value));
-
-  // lsp初始化消息
-  lspBridge.onInitialize((parameters) async {
-    // 请求原始的dart lsp初始化方法，必须要请求，否则lsp无法工作
-    await sendDartLspRequest('initialize', parameters.toJson());
-    // 返回固定的结果
-    return InitializeResult.fromJson(initializeResultJson);
-  });
-
-  // lsp结束消息
-  lspBridge.onShutdown(() async {
-    await sendDartLspRequest('shutdown', {});
-    dartLsp.close();
-    lspBridge.close();
-    dartProcess.kill();
-  });
-
-  // 监听2个
-  dartLsp.listen();
-  await lspBridge.listen();
 }
-
-/// 一段固定的返回结果（数据来自zed启动dart返回的结果，并增加了一个`"triggerCharacters":["."]`字段）
-// TODO: 这个随zed或者dart的更新，可能需要定期更新吧？？？
-final initializeResultJson = jsonDecode('''{
-  "capabilities": {
-    "textDocumentSync": {"change": 2},
-    "selectionRangeProvider": true,
-    "hoverProvider": {},
-    "completionProvider": {
-      "resolveProvider": true,
-      "triggerCharacters": ["."]
-    },
-    "signatureHelpProvider": {
-      "triggerCharacters": ["("],
-      "retriggerCharacters": [","]
-    },
-    "definitionProvider": {},
-    "typeDefinitionProvider": true,
-    "implementationProvider": true,
-    "referencesProvider": true,
-    "documentHighlightProvider": true,
-    "documentSymbolProvider": {},
-    "workspaceSymbolProvider": true,
-    "codeActionProvider": {
-      "codeActionKinds": [
-        "source",
-        "source.organizeImports",
-        "source.fixAll",
-        "source.sortMembers",
-        "quickfix",
-        "refactor"
-      ]
-    },
-    "codeLensProvider": {},
-    "documentFormattingProvider": {},
-    "documentRangeFormattingProvider": {},
-    "documentOnTypeFormattingProvider": {
-      "firstTriggerCharacter": "}",
-      "moreTriggerCharacter": [";"]
-    },
-    "renameProvider": {"prepareProvider": true},
-    "documentLinkProvider": {"resolveProvider": false},
-    "colorProvider": {},
-    "foldingRangeProvider": true,
-    "executeCommandProvider": {
-      "commands": [
-        "dart.edit.sortMembers",
-        "dart.edit.organizeImports",
-        "dart.edit.fixAll",
-        "dart.edit.fixAllInWorkspace.preview",
-        "dart.edit.fixAllInWorkspace",
-        "dart.edit.sendWorkspaceEdit",
-        "refactor.perform",
-        "refactor.validate",
-        "dart.logAction",
-        "dart.refactor.convert_all_formal_parameters_to_named",
-        "dart.refactor.convert_selected_formal_parameters_to_named",
-        "dart.refactor.move_selected_formal_parameters_left",
-        "dart.refactor.move_top_level_to_file"
-      ],
-      "workDoneProgress": true
-    },
-    "workspace": {
-      "workspaceFolders": {"supported": true, "changeNotifications": true}
-    },
-    "callHierarchyProvider": true,
-    "semanticTokensProvider": {
-      "legend": {
-        "tokenTypes": [
-          "annotation",
-          "keyword",
-          "class",
-          "comment",
-          "method",
-          "variable",
-          "parameter",
-          "enum",
-          "enumMember",
-          "type",
-          "source",
-          "property",
-          "namespace",
-          "boolean",
-          "number",
-          "string",
-          "function",
-          "typeParameter"
-        ],
-        "tokenModifiers": [
-          "documentation",
-          "constructor",
-          "declaration",
-          "importPrefix",
-          "instance",
-          "static",
-          "escape",
-          "annotation",
-          "control",
-          "label",
-          "interpolation",
-          "void"
-        ]
-      },
-      "range": true,
-      "full": {"delta": false}
-    },
-    "inlayHintProvider": {"resolveProvider": false},
-    "experimental": {
-      "textDocument": {"super": {}, "augmented": {}, "augmentation": {}}
-    }
-  }
-}''');
